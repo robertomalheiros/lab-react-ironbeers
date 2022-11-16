4d724fad82fb4618642b9c8605c44140fd1e26d8
robertomalheiros <robertomalheirosb@gmail.com>
2022-11-16 11:40:54 -0300

    Random

--- src/components/template/RandomBeer.js
+++ src/components/template/RandomBeer.js
@@ -2,17 +2,41 @@ import React from "react";
 
 import Irandombeer from "../../assets/imgs/random-beer.png";
 import Main from "../template/Main";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function RandomBeer() {
+  const [cervejas, setcervejas] = useState([]);
+  const [reload] = useState(false);
+
+  useEffect(() => {
+    async function fetchStudents() {
+      const response = await axios.get("https://ironbeer-api.fly.dev/random");
+      setcervejas(response.data);
+    }
+
+    fetchStudents();
+  }, [reload]);
+
   return (
-    <Main>
+    <Main className="MainAll">
       <div className="ImgLogo">
         <img src={Irandombeer} alt="logo" />
       </div>
-      <div className="AllBeersText">
-        <h3>Cerveja Aleatória</h3>
-        <hr />
-        <p>Cerveja Selecionada</p>
+      <div className="cerveja" key={cervejas._id}>
+        <div>
+          <Link to={`/detalhes-beer/${cervejas._id}`}>
+            <img className="imgCervejas" src={cervejas.image} alt="logo" />
+          </Link>
+        </div>
+        <div className="AllBeersText">
+          <h4>{cervejas.nome}</h4>
+          <p>{cervejas.tagline}</p>
+          <p>
+            <bold> Created by:</bold> {cervejas.contributed_by}
+          </p>
+        </div>
       </div>
     </Main>
   );
